af345caf5df28d61b54c1f69be8d81dfae4d5587
unknown <bhanusankhyan77@gmail.com>
2023-08-07 11:57:17 +0530

    ui fixes v_1.0

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -61,9 +61,9 @@ const Home = () => {
       <br />
     </center>
     { loader ?
-      <div className="d-flex justify-content-center" style={{marginTop:'20%'}}>
+      <div className="d-flex justify-content-center" style={{marginTop:'10%'}}>
       <div className="spinner-grow" style={{width: '3rem', height: '3rem', role:"status"}}>
-        <span className="sr-only">Loading...</span>
+        <span className="sr-only"></span>
       </div>
        </div>
        :
